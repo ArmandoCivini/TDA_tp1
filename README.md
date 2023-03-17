9c7930077d143b2b78e0c2c56cabc537df8652ba
Tomas Civini <tcivini@gmail.com>
2023-03-17 12:23:35 -0300

    added the iteration to check all posible matches

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # TDA_tp1
+
+example1: 1 solo punto en B dominado por 2 de a, 1 solo match
+example2: el primero de B dominado por 2 de a y el segundo dominado por solo 1
+example3: el primero de B solo tiene un match asi que se va a agregar a la lista, el segundo tiene 2 matches pero como el primero ya tomo ese punto deberia matchear con su segundo
+example4: ahora el que tiene dos matches es el primer punto que se procesa
+example5: 2 puntos en B con dos matches cada uno
--- matches.py
+++ matches.py
@@ -1,5 +1,41 @@
 from parser import parser
+import sys
 
-A, B = parser('example1.txt')
-print(A)
-print(B)
+def is_dominant(a, b):
+    return a[0] >= b[0] and a[1] >= b[1]
+
+def search_single_match(posible_matches):
+    for posible_match in posible_matches:
+        if len(posible_match[1]) == 1:
+            return posible_match
+    return None
+
+def recursive_single_matches(point_match, point, posible_matches, matches, A):
+    matches.append((point_match, point))
+    A.remove(point_match)
+    for posible_match in posible_matches:
+        posible_match[1].discard(point_match)
+    posible_match = search_single_match(posible_matches)
+    if posible_match:
+        matches, posible_matches, A = recursive_single_matches(posible_match[1].pop(), posible_match[0], posible_matches, matches, A)
+    return matches, posible_matches, A
+
+path = sys.argv[1]
+A, B = parser(path)
+#supuesto no hay puntos repetidos
+posible_matches = []
+matches = []
+for point in B: 
+    point_matches = set()
+    for point2 in A: #busco los matches posibles para point y los agrego a un set
+        if is_dominant(point2, point):
+            point_matches.add(point2)
+    if len(point_matches) == 1: #si hay un solo match, lo agrego a matches y saco ese punto de posible_matches y de A
+        #ademas itero recursivamente para ir agregando todos los puntos que vayan quedando con un solo match
+        matches, posible_matches, A = recursive_single_matches(point_matches.pop(), point, posible_matches, matches, A)
+    elif len(point_matches) > 1:
+        #si tiene mas de un match solo se agrega a la lista de posibles matches
+        posible_matches.append((point, point_matches))
+
+print(posible_matches)
+print(matches)
